f6908db855c1888f98bcc343c48c29c6b139d4a7
VicthorSousa <89492328+VicthorSousa@users.noreply.github.com>
2024-03-05 16:16:13 -0300

    teste.py

--- teste.py
+++ teste.py
@@ -24,5 +24,5 @@ new_df2['Total'] = new_df['CUPNUM'].nunique()
 new_df2['Percentual'] = (new_df2['CUPNUM'] / new_df2['Total'] * 100).round(2).astype(str) + '%'
 new_df2.drop(['Qnt Venda', 'Total', 'CODPRD', 'CODBAR', 'CUPNUM'], axis=1, inplace=True)
 
-st.text(f'Analise de {quantidade} cupons.')
+st.text(f'Analise de {quantidade} cupons emitidos do produto selecionado.')
 st.dataframe(new_df2, use_container_width=True)
