78f90a4dc129369476b1b4024aaba1b08f50ba2c
VicthorSousa <89492328+VicthorSousa@users.noreply.github.com>
2024-03-06 15:56:32 -0300

    teste.py

--- teste.py
+++ teste.py
@@ -22,7 +22,7 @@ new_df1.rename(columns={'CUPNUM': 'Qnt Cupons'})
 new_df2 = new_df1.sort_values(('CUPNUM'), ascending=False).head(500)
 new_df2['Total'] = new_df['CUPNUM'].nunique()
 new_df2['Percentual'] = (new_df2['CUPNUM'] / new_df2['Total'] * 100).round(2).astype(str) + '%'
-new_df2.drop(['Qnt Venda', 'Total', 'CODPRD', 'CUPNUM'], axis=1, inplace=True)
+new_df2.drop(['Qnt Venda', 'Total', 'CODPRD', 'CODBAR', 'CUPNUM'], axis=1, inplace=True)
 
 st.text(f'Analise de {quantidade} cupons emitidos do produto selecionado.')
 st.dataframe(new_df2, use_container_width=True)
