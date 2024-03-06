4188163a95a0ae184d1374c1716ee9250ca40d2e
VicthorSousa <89492328+VicthorSousa@users.noreply.github.com>
2024-03-06 15:51:19 -0300

    teste.py

--- teste.py
+++ teste.py
@@ -3,17 +3,17 @@ import pandas as pd
 
 pd.options.display.float_format = '{:.0f}'.format
 
-df = pd.read_csv('cupons_janeiro_filial.csv', on_bad_lines='skip', sep=';')
+df = pd.read_csv('cupons_janeiro_matriz.csv', on_bad_lines='skip', sep=';')
 produto_lista = df['Produto'].sort_values().unique()
 
-st.title('Análise de Produtos - Coocorrência')
+st.title('Análise de Produtos - Coocorrência - Loja Avenida')
 option = st.selectbox(label='Selecione o produto', options=produto_lista,placeholder='Selecione o produto...')
 #nome_produto = st.markdown(sorted(option))
 
 
 
 qnt_total = df['CUPNUM'].nunique()                      
-df1 = df.loc[(df['Produto']==option), ['CUPNUM','CODPRD', 'CODBAR', 'Produto', 'Qnt Venda', 'Total']]
+df1 = df.loc[(df['Produto']==option), ['CUPNUM','CODPRD', 'Produto', 'Qnt Venda', 'Total']]
 cupons_lista = df1['CUPNUM'].tolist()
 new_df = df.loc[df['CUPNUM'].isin(cupons_lista)]
 quantidade = new_df['CUPNUM'].nunique()
@@ -22,7 +22,7 @@ new_df1.rename(columns={'CUPNUM': 'Qnt Cupons'})
 new_df2 = new_df1.sort_values(('CUPNUM'), ascending=False).head(500)
 new_df2['Total'] = new_df['CUPNUM'].nunique()
 new_df2['Percentual'] = (new_df2['CUPNUM'] / new_df2['Total'] * 100).round(2).astype(str) + '%'
-new_df2.drop(['Qnt Venda', 'Total', 'CODPRD', 'CODBAR', 'CUPNUM'], axis=1, inplace=True)
+new_df2.drop(['Qnt Venda', 'Total', 'CODPRD', 'CUPNUM'], axis=1, inplace=True)
 
 st.text(f'Analise de {quantidade} cupons emitidos do produto selecionado.')
 st.dataframe(new_df2, use_container_width=True)
